b99d0d72e68dab0c0873ce42a9edb0c987835924
chpplen <chpplen@hotmail.com>
2014-04-23 13:12:01 +0800

    set edit method

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,19 @@
 class ArticlesController < ApplicationController
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
 	@article = Article.find(params[:id])
   end
